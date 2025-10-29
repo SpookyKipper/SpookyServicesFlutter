3bee348d818ea04fb739acca3d61701650da37ac
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2025-10-29 13:59:59 +0800

    Refactor language helper methods for better readability and consistency

--- lib/helpers/language.dart
+++ lib/helpers/language.dart
@@ -14,6 +14,12 @@ class LangHelper {
 
   String lt(String textChi, String textEn) => lh.locText(textChi, textEn);
 
+  String locTextList(List<String> texts) {
+    return locText(texts[0], texts[1]);
+  }
+
+  String ltl(List<String> texts) => lh.locTextList(texts);
+
   void setLang(String lang) {
     language = lang;
     sp.setString('language', lang);
