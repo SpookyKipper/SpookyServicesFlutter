e1e56bad73bdfbbd34c76c8748a5ee65258f0700
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2025-10-29 12:30:11 +0800

    alias

--- lib/helpers/language.dart
+++ lib/helpers/language.dart
@@ -3,7 +3,8 @@ import 'package:spookyservices/helpers/shared_pref.dart';
 class LangHelper {
   String language = sp.getString('spookysrv.language') ?? 'en';
 
-  String LocText(String textChi, String textEn) { //Localized Text
+  String locText(String textChi, String textEn) {
+    //Localized Text
     if (language == 'en') {
       return textEn;
     } else {
@@ -11,11 +12,12 @@ class LangHelper {
     }
   }
 
+  String lt(String textChi, String textEn) => lh.locText(textChi, textEn);
+
   void setLang(String lang) {
     language = lang;
     sp.setString('language', lang);
   }
-
 
   String getLang() {
     return sp.getString("spookysrv.language") ?? 'en';
